b6b6c9c77f26bb1cd778cb848be7b3117da84fc7
Luke <luke.kazungu@student.moringaschool.com>
2022-10-03 03:14:50 +0300

    Assignment completed

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,14 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    let change =()=>{console.log('Entering password...')}
     return (
-        <div></div>
+        <div>
+            <input 
+            type="password"
+            onChange={change}
+            />
+        </div>
     )
 }
 
